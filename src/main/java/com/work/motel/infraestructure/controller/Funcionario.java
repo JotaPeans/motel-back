65611aea6155a39ee38161c200c9368b7574d221
JotaPeans <jpfontesferreira1@gmail.com>
2025-04-03 10:49:19 -0300

    env db

--- src/main/java/com/work/motel/infraestructure/controller/Funcionario.java
+++ src/main/java/com/work/motel/infraestructure/controller/Funcionario.java
@@ -1,4 +1,4 @@
-package com.work.motel.controller;
+package com.work.motel.infraestructure.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
