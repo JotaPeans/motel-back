d2b152f4d0851f26a20b62207b9896e9b54b9133
JotaPeans <jpfontesferreira1@gmail.com>
2025-04-18 01:11:38 -0300

    remove imports

--- src/main/java/com/work/motel/application/util/JwtUtil.java
+++ src/main/java/com/work/motel/application/util/JwtUtil.java
@@ -2,7 +2,6 @@ package com.work.motel.application.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
